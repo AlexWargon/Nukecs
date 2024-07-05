d9217dceae0b95e30e88bfe1a30a40a195e51198
Alex Wargon <wargonger@gmail.com>
2024-07-05 21:13:48 +0300

    initial

---
+++ Query.cs
@@ -0,0 +1,126 @@
+﻿namespace Wargon.Nukecs {
+    public unsafe struct Query {
+        internal unsafe struct QueryImpl {
+            internal BitMask with;
+            internal BitMask none;
+            internal Unity.Collections.LowLevel.Unsafe.UnsafeList<int> entities;
+            internal Unity.Collections.LowLevel.Unsafe.UnsafeList<int> entitiesMap;
+            internal int count;
+            internal readonly World.WorldImpl* world;
+            internal readonly QueryImpl* self;
+
+            internal static QueryImpl* Create(World.WorldImpl* world) {
+                var ptr = Unsafe.Malloc<QueryImpl>(world->allocator);
+                *ptr = new QueryImpl(world, ptr);
+                return ptr;
+            }
+
+            internal QueryImpl(World.WorldImpl* world, QueryImpl* self) {
+                this.world = world;
+                this.with = default;
+                this.none = default;
+                this.count = default;
+                this.entities = new Unity.Collections.LowLevel.Unsafe.UnsafeList<int>(world->config.StartEntitiesAmount, world->allocator);
+                this.entitiesMap = new Unity.Collections.LowLevel.Unsafe.UnsafeList<int>(world->config.StartEntitiesAmount, world->allocator);
+                this.self = self;
+            }
+            public ref Entity GetEntity(int index) {
+                return ref world->GetEntity(entities[index]);
+            }
+            internal bool Has(int entity) {
+                if (entitiesMap.Length <= entity) return false;
+                return entitiesMap[entity] > 0;
+            }
+
+            internal void Add(int entity) {
+                if (entities.Length - 1 <= count) {
+                    entities.Resize(count * 2);
+                }
+                if (entitiesMap.Length - 1 <= entity) {
+                    entitiesMap.Resize(count * 2);
+                }
+                entities[count++] = entity;
+                entitiesMap[entity] = count;
+            }
+            internal void Remove(int entity) {
+                if (!Has(entity)) return;
+                var index = entitiesMap[entity] - 1;
+                entitiesMap[entity] = 0;
+                count--;
+                if (count > index) {
+                    entities[index] = entities[count];
+                    entitiesMap[entities[index]] = index + 1;
+                }
+            }
+
+            public QueryImpl* With(int type) {
+                with.Add(type);
+                return self;
+            }
+
+            public bool HasWith(int type) {
+                return with.Has(type);
+            }
+
+            public bool HasNone(int type) {
+                return none.Has(type);
+            }
+            public QueryImpl* None(int type) {
+                none.Add(type);
+                return self;
+            }
+        }
+
+        internal readonly QueryImpl* impl;
+
+        internal Query(World.WorldImpl* world) {
+            impl = QueryImpl.Create(world);
+        }
+
+        internal Query(QueryImpl* impl) {
+            this.impl = impl;
+        }
+
+        public Query With<T>(){
+            impl->With(ComponentMeta<T>.Index);
+            return this;
+        }
+
+        public Query None<T>() {
+            impl->None(ComponentMeta<T>.Index);
+            return this;
+        }
+
+        public ref Entity GetEntity(int index) {
+            return ref impl->GetEntity(index);
+        }
+    }
+
+    public struct BitMask {
+        public long mask;
+        public int count;
+        const int BitSize = (sizeof(int) * 8) - 1;
+        const int ByteSize = 5;  // log_2(BitSize + 1)
+
+        public static BitMask New() {
+            BitMask mask;
+            mask.count = 0;
+            mask.mask = -1;
+            return mask;
+        }
+        public bool Add(int item) {
+            if (Has(item)) return false;
+            mask |= (long)(1 << item);
+            count++;
+            return true;
+        }
+
+        public bool Has(int item) {
+            return (mask & (long)(1 << item)) > 0;
+        }
+    }
+
+    public struct Bit {
+        public int Value;
+    }
+}
---
+++ World.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+namespace Wargon.Nukecs {
+    public unsafe struct World {
+        private static World[] worlds = new World[4];
+        public static ref World Get(int index) => ref worlds[index];
+
+        public static World Craete() {
+            World world;
+            world._impl = WorldImpl.Create(worlds.Length, WorldConfig.Default);
+            return world;
+        }
+
+        private WorldImpl* _impl;
+
+        public Entity CreateEntity() {
+            return _impl->CreateEntity();
+        }
+
+        public ref Entity GetEntity(int id) {
+            return ref _impl->GetEntity(id);
+        }
+
+        public Query CreateQuery() {
+            return new Query(_impl->CreateQuery());
+        }
+        public ref UntypedUnsafeList GetPool<T>() where T : unmanaged => ref _impl->GetPool<T>();
+        internal unsafe struct WorldImpl {
+            internal readonly int Id;
+            internal readonly Allocator allocator;
+            internal readonly UnsafeList<Entity> entities;
+            internal readonly UntypedUnsafeList pools;
+            internal readonly UnsafePtrList<Query.QueryImpl> queries;
+            internal UnsafeHashMap<int, Archetype> archetypesMap;
+            internal UnsafePtrList<Archetype.ArchetypeImpl> archetypesList;
+            internal int lastEntityIndex;
+            internal readonly WorldConfig config;
+            internal readonly WorldImpl* self;
+            internal static WorldImpl* Create(int id, WorldConfig config) {
+                var ptr = Unsafe.Malloc<WorldImpl>(Allocator.Persistent);
+                *ptr = new WorldImpl(id, config, Allocator.Persistent, ptr);
+                return ptr;
+            }
+
+            internal Query.QueryImpl* CreateQuery() {
+                var ptr = Query.QueryImpl.Create(self);
+                queries.Add(ptr);
+                return ptr;
+            }
+            public WorldImpl(int id, WorldConfig config, Allocator allocator, WorldImpl* self) {
+                
+                this.Id = id;
+                this.allocator = allocator;
+                this.entities = new UnsafeList<Entity>(config.StartEntitiesAmount, allocator);
+                this.pools = new UntypedUnsafeList(typeof(UntypedUnsafeList),config.StartComponentsAmount, allocator);
+                this.queries = new UnsafePtrList<Query.QueryImpl>(32, allocator);
+                this.archetypesList = new UnsafePtrList<Archetype.ArchetypeImpl>(32, allocator);
+                this.archetypesMap = new UnsafeHashMap<int, Archetype>(32, allocator);
+                this.lastEntityIndex = 0;
+                this.config = config;
+                this.self = self;
+                CreateArchetype();
+            }
+            internal ref UntypedUnsafeList GetPool<T>() {
+                var poolIndex = ComponentMeta<T>.Index;
+                ref var pool = ref pools.GetRef<UntypedUnsafeList>(poolIndex);
+                if (pool.IsCreated == false) {
+                    pool = new UntypedUnsafeList(typeof(T), config.StartPoolSize, allocator);
+                    pools.Set(poolIndex, ref pool);
+                }
+
+                return ref pool;
+            }
+
+            internal Entity CreateEntity() {
+                var e = new Entity(lastEntityIndex, self);
+                entities.ElementAt(lastEntityIndex) = e;
+                lastEntityIndex++;
+                return e;
+            }
+
+            internal ref Entity GetEntity(int id) {
+                return ref entities.ElementAt(id);
+            }
+
+            public Archetype CreateArchetype(params int[] types) {
+                var ptr = Archetype.ArchetypeImpl.Create(self, types);
+                Archetype archetype;
+                archetype._impl = ptr;
+                archetypesList.Add(ptr);
+                archetypesMap[ptr->id] = archetype;
+                return archetype;
+            }
+
+            private Archetype CreateArchetype() {
+                var ptr = Archetype.ArchetypeImpl.Create(self);
+                Archetype archetype;
+                archetype._impl = ptr;
+                archetypesList.Add(ptr);
+                archetypesMap[ptr->id] = archetype;
+                return archetype;
+            }
+
+            internal Archetype GetArchetype(int hash) {
+                return archetypesMap[hash];
+            }
+        }
+
+    }
+    
+    public struct WorldConfig {
+        public int StartEntitiesAmount;
+        public int StartPoolSize;
+        public int StartComponentsAmount;
+        public Allocator WorldAllocator => Allocator.Persistent;
+
+        public static WorldConfig Default => new WorldConfig() {
+            StartPoolSize = 128,
+            StartEntitiesAmount = 128,
+            StartComponentsAmount = 32
+        };
+    }
+}
