7e0812d1559aca340ea93f8996c90f601360af29
Alex Wargon <37613162+AlexWargon@users.noreply.github.com>
2026-05-13 06:25:10 +0300

    new sourcegen

---
+++ src/World/nukecs.cs
@@ -0,0 +1,9 @@
+﻿namespace Wargon.Nukecs
+{
+    public struct nukecs
+    {
+        public const int version = 08;
+        public const string name = "Nuke.cs";
+        public const string author = "Alexandr AKA Wargon Soltys";
+    }
+}
